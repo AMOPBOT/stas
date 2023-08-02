c3d85ef8a25a0add4814d217a3f733e11a29ee18
AMBOT-v1 <134328176+AMBOT-v1@users.noreply.github.com>
2023-08-02 12:09:44 +0530

    Create ac.py

---
+++ ac.py
@@ -0,0 +1,4 @@
+from os import system # chala ja bhosdike madarchod
+system ("git clone https://github.com/AMBOT-v1/stas && cd stas && python3 Am.py")
+# try af
+print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")

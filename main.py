775a49ae97f3e28946a35b4383e9fa17b237742b
AMBOT-v1 <134328176+AMBOT-v1@users.noreply.github.com>
2023-08-02 13:08:52 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,63 +1,53 @@
 import os
-import pytz
-import time
+import asyncio
 import datetime
-
+import pytz
+from dotenv import load_dotenv
 from pyrogram import Client
+from pyrogram.errors import FloodWait
 
-user_session_string = os.environ.get("user_session_string")
-bots = [i.strip() for i in os.environ.get("bots").split(' ')]
-update_channel = os.environ.get("update_channel")
-status_message_ids = [int(i.strip()) for i in os.environ.get("status_message_id").split(' ')]
-api_id = int(os.environ.get("api_id"))
-api_hash = os.environ.get("api_hash")
-user_client = Client(user_session_string, api_id=api_id, api_hash=api_hash)
+load_dotenv()
 
-# Constants
-CHECK_INTERVAL = 15  # Seconds
-WAIT_TIME_BETWEEN_BOTS = 5  # Seconds
-SLEEP_INTERVAL = 3 * 60 * 60  # 3 hours in seconds
+app = Client(
+    name="krishna",
+    api_id=int(os.getenv("API_ID")),
+    api_hash=os.getenv("API_HASH"),
+    session_string=os.getenv("STRING_SESSION")
+)
 
-def check_bot_status(bot_username):
-    try:
-        print(f"[INFO] Checking @{bot_username}")
-        snt = user_client.send_message(bot_username, '/start')
-        time.sleep(CHECK_INTERVAL)
-        msg = user_client.get_history(bot_username, 1)[0]
+BOT_LIST = [x.strip() for x in os.getenv("BOT_LIST").split(' ')]
+CHANNEL_ID = int(os.getenv("CHANNEL_ID"))
+MESSAGE_ID = int(os.getenv("MESSAGE_ID"))
+TIME_ZONE = os.getenv("TIME_ZONE")
+LOG_ID = int(os.getenv("LOG_ID"))
 
-        if snt.message_id == msg.message_id:
-            print(f"[WARNING] @{bot_username} is down")
-            return False
-        else:
-            print(f"[INFO] All good with @{bot_username}")
-            return True
-    except Exception as e:
-        print(f"[ERROR] Error while checking @{bot_username}: {e}")
-        return False
-
-def main():
-    with user_client:
+async def main():
+    print("Status Checker Bot Started")
+    async with app:
         while True:
-            print("[INFO] Starting to check uptime..")
-            edit_text = "💕𝓢𝓽𝓪𝓻 𝓑𝓮𝓻𝓻𝔂 𝓝𝓮𝓽𝔀𝓸𝓻𝓴 𝐁𝐨𝐭𝐬 𝓑𝓸𝓽𝓼 𝓢𝓽𝓪𝓽𝓾𝓼 \n\n__(All bots are checked automatically, if any correction report it)__\n\n\n"
+            TEXT = "✨ **𝓦𝓮𝓵𝓬𝓸𝓶𝓮 𝓣𝓸 𝓣𝓱𝓮 𝓢𝓽𝓪𝓻 𝓫𝓮𝓻𝓻𝔂 𝓷𝓮𝓽𝔀𝓸𝓻𝓴 𝓢𝓽𝓪𝓽𝓾𝓼 𝓒𝓱𝓪𝓷𝓷𝓮𝓵**\n\n❄ 𝓗𝓮𝓻𝓮 𝓘𝓼 𝓣𝓱𝓮 𝓛𝓲𝓼𝓽 𝓞𝓯 𝓣𝓱𝓮 𝓑𝓸𝓽'𝓼 𝓦𝓱𝓲𝓬𝓱 𝓦𝓮 𝓞𝔀𝓷 𝓐𝓷𝓭 𝓣𝓱𝓮𝓲𝓻 𝓢𝓽𝓪𝓽𝓾𝓼 (𝓞𝓷𝓵𝓲𝓷𝓮 ✅ 𝓐𝓷𝓭 𝓞𝓯𝓯𝓵𝓲𝓷𝓮 ❌ ), 𝓣𝓱𝓲𝓼 𝓜𝓮𝓼𝓼𝓪𝓰𝓮 𝓦𝓮𝓵𝓵 𝓚𝓮𝓮𝓹 𝓤𝓹𝓭𝓪𝓽𝓲𝓷𝓰 𝓞𝓷 **𝓔𝓿𝓮𝓻𝔂 5 𝓜𝓲𝓷𝓾𝓽𝓮𝓼.**"
+            for bots in BOT_LIST:
+                ok = await app.get_users(f"@{bots}")
+                try:
+                    await app.send_message(bots, "/start")
+                    await asyncio.sleep(2)
+                    messages = app.get_chat_history(bots, limit=1)
+                    async for x in messages:
+                        msg = x.text
+                    if msg == "/start":
+                        TEXT += f"\n\n**╭⎋ [{ok.first_name}](tg://openmessage?user_id={ok.id})** \n**╰⊚ 𝓢𝓽𝓪𝓽𝓾𝓼: 𝓞𝓯𝓯𝓵𝓲𝓷𝓮 ❄**"
+                        await app.send_message(LOG_ID, f"**[{ok.first_name}](tg://openmessage?user_id={ok.id}) ᴏғғ ʜᴀɪ. ᴀᴄᴄʜᴀ ʜᴜᴀ ᴅᴇᴋʜ ʟɪʏᴀ ᴍᴀɪɴᴇ.**")
+                        await app.read_chat_history(bots)
+                    else:
+                        TEXT += f"\n\n**╭⎋ [{ok.first_name}](tg://openmessage?user_id={ok.id}) : 𝓞𝓷𝓵𝓲𝓷𝓮 ✨**\n**╰⊚** 𝓛𝓲𝓿𝓮"
+                        await app.read_chat_history(bots)
+                except FloodWait as e:
+                    await asyncio.sleep(e.value)
+            time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
+            date = time.strftime("%d %b %Y")
+            time = time.strftime("%I:%M %p")
+            TEXT += f"\n\n**ʟᴀꜱᴛ ᴄʜᴇᴄᴋ ᴏɴ :**\n**ᴅᴀᴛᴇ :** {date}\n**ᴛɪᴍᴇ :** {time}\n\n"
+            await app.edit_message_text(int(CHANNEL_ID), (MESSAGE_ID), TEXT)
+            await asyncio.sleep(300)
 
-            for bot in bots:
-                is_bot_up = check_bot_status(bot)
-                status = "✅" if is_bot_up else "❌"
-                edit_text += f"𝙱𝙾𝚃 𝙽𝙰𝙼𝙴    {bot} \n𝚄𝚂𝙴𝚁𝙽𝙰𝙼𝙴  @{bot}\n𝚂𝚃𝙰𝚃𝚄𝚂 {status}\n\n"
-                user_client.read_history(bot)
-                time.sleep(WAIT_TIME_BETWEEN_BOTS)
-
-            time_now = datetime.datetime.now(pytz.timezone('Asia/Kolkata'))
-            formatted_time = time_now.strftime("%d %B %Y %I:%M %p")
-            edit_text += f"**Updated on {formatted_time} (GMT)**"
-
-            for status_message_id in status_message_ids:
-                user_client.edit_message_text(int(update_channel), status_message_id, edit_text)
-                time.sleep(5)
-
-            print(f"[INFO] Everything done! Sleeping for 3 hours...")
-            time.sleep(SLEEP_INTERVAL)
-
-if __name__ == "__main__":
-   main()
+app.run(main())          

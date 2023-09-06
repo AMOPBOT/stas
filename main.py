374c6e203a6d3ea0c374030446ec9b6ce42cfae8
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-09-06 23:18:13 +0530

    Update main.py

--- main.py
+++ main.py
@@ -25,7 +25,7 @@ async def main():
     print("Status Checker Bot Started")
     async with app:
         while True:
-            TEXT = "✨ 𝗪𝗲𝗹𝗹𝗖𝗼𝗺𝗲 𝗧𝗼 𝗧𝗵𝗲 𝗦𝘁𝗮𝗿 𝗕𝗲𝗿𝗿𝘆 𝗡𝗲𝘁𝗪𝗼𝗿𝗸.**\n\n\n❄ Here Is The List Of The Bots.\n❄ Which We Own And Their Status\n\n𝓞𝓷𝓵𝓲𝓷𝓮 ✅\n 𝓞𝓯𝓯𝓵𝓲𝓷𝓮 ❌ \n\nThis Messege Well Keep Updating On Every 5 Minutes."
+            TEXT = "✨ 𝗪𝗲𝗹𝗹𝗖𝗼𝗺𝗲 𝗧𝗼 𝗧𝗵𝗲 𝗦𝘁𝗮𝗿 𝗕𝗲𝗿𝗿𝘆 𝗡𝗲𝘁𝗪𝗼𝗿𝗸.**\n\n\n❄ Here Is The List Of The Bots.\n❄ Which We Own And Their Status\n\n𝓞𝓷𝓵𝓲𝓷𝓮 ✅\n𝓞𝓯𝓯𝓵𝓲𝓷𝓮 ❌ \n\nThis Messege Well Keep Updating On Every 5 Minutes."
             for bots in BOT_LIST:
                 ok = await app.get_users(f"@{bots}")
                 try:

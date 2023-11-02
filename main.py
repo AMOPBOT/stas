64024dea8aa77a1f60115f812a933018ba242034
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-02 15:11:54 +0530

    Update main.py

--- main.py
+++ main.py
@@ -29,7 +29,7 @@ async def main():
             cpu_percent = psutil.cpu_percent()
             ram_percent = psutil.virtual_memory().percent
             disk_usage = psutil.disk_usage('/')
-            TEXT = " Here Is The List Of The Bots.\n❄ Which We Own And Their Status\n\nOnline ✅\nOffline ❌ \n\nThis Messege Well Keep Updating On Every 30 Minutes."
+            TEXT = " Here Is The List Of The Bots.\n❄ Which We Own And Their Status\n\nOnline ✅\nOffline ❌\n\nThis Message Will Keep Updating Every 30 Minutes."
             for bots in BOT_LIST:
                 ok = await app.get_users(f"@{bots}")
                 try:
@@ -39,19 +39,19 @@ async def main():
                     async for x in messages:
                         msg = x.text
                     if msg == "/start":
-                         TEXT += f"\n\n**╭⎋ [{ok.first_name}](tg://openmessage?user_id={ok.id})** \n**╰⊚ 𝓢𝓽𝓪𝓽𝓾𝓼:  ❌\n\nSystem Info:**\nCPU Usage: {cpu_percent}%\nRAM Usage: {ram_percent}%\nStorage Usage: {disk_usage.percent}%\n**"
+                        TEXT += f"\n\n**╭⎋ [{ok.first_name}](tg://openmessage?user_id={ok.id})**\n**╰⊚ 𝓢𝓽𝓪𝓽𝓾𝓼:  ❌\n\nSystem Info:\nCPU Usage: {cpu_percent}%\nRAM Usage: {ram_percent}%\nStorage Usage: {disk_usage.percent}%\n"
                         await app.send_message(LOG_ID, f"𝓢𝓲𝓻 **[{ok.first_name}](tg://openmessage?user_id={ok.id}) 𝓞𝓯𝓯 𝓗𝓮..**")
                         await app.read_chat_history(bots)
                     else:
-                         TEXT += f"\n\n**╭⎋ [{ok.first_name}](tg://openmessage?user_id={ok.id})**\n**╰⊚** 𝓢𝓽𝓪𝓽𝓾𝓼:  ✅\n\nSystem Info:**\nCPU Usage: {cpu_percent}%\nRAM Usage: {ram_percent}%\nStorage Usage: {disk_usage.percent}%\n"
+                        TEXT += f"\n\n**╭⎋ [{ok.first_name}](tg://openmessage?user_id={ok.id})**\n**╰⊚ 𝓢ᓿtatus:  ✅\n\nSystem Info:\nCPU Usage: {cpu_percent}%\nRAM Usage: {ram_percent}%\nStorage Usage: {disk_usage.percent}%\n"
                         await app.read_chat_history(bots)
                 except FloodWait as e:
                     await asyncio.sleep(e.value)
             time = datetime.datetime.now(pytz.timezone(f"{TIME_ZONE}"))
             date = time.strftime("%d %b %Y")
             time = time.strftime("%I:%M %p")
-            TEXT += f"\n\n**ʟᴀꜱᴛ ᴄʜᴇᴄᴋ ᴏɴ :**\n**ᴅᴀᴛᴇ :** {date}\n**ᴛɪᴍᴇ :** {time}\nɴᴇᴛᴡᴏʀᴋ ᴏꜰ : ꜱᴏᴏɴ..."
-            await app.edit_message_text(int(CHANNEL_ID), (MESSAGE_ID), TEXT)
-            await asyncio.sleep(1800)
+            TEXT += f"\n\n**Last check on:**\n**Date:** {date}\n**Time:** {time}\nNetwork status: Soon..."
+            await app.edit_message_text(int(CHANNEL_ID), MESSAGE_ID, TEXT)
+            await asyncio.sleep(1800)  # Sleep for 30 minutes (1800 seconds)
 
-app.run(main())          
+app.run(main())

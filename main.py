fde6bec1d7b27916fb183b92c736be3987b6180d
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-02 18:52:05 +0530

    Update main.py

--- main.py
+++ main.py
@@ -58,14 +58,14 @@ async def main_pratheek():
                         await app.read_chat_history(bot)
                     else:
                         xxx_pratheek += f"\n\n🤖  @{bot}\n        └ **Alive** ✅\n"
-                        xxx_pratheek += f"Total CPU Usage: {total_cpu_percent}%\n"
-                        xxx_pratheek += f"Total CPU Cores: {total_cpu_cores}\n"
-                        xxx_pratheek += f"Total RAM: {total_ram / (1024 ** 3):.2f} GB\n"
-                        xxx_pratheek += f"Used RAM: {used_ram / (1024 ** 3):.2f} GB\n"
-                        xxx_pratheek += f"RAM Usage: {ram_percent}%\n"
-                        xxx_pratheek += f"Total ROM: {total_rom / (1024 ** 3):.2f} GB\n"
-                        xxx_pratheek += f"Used ROM: {used_rom / (1024 ** 3):.2f} GB\n"
-                        xxx_pratheek += f"ROM Usage: {disk_percent}%\n"
+                        xxx_pratheek += f"╭⎋ Total CPU Usage: {total_cpu_percent}%\n"
+                        xxx_pratheek += f"╰⊚ Total CPU Cores: {total_cpu_cores}\n"
+                        xxx_pratheek += f"╭⎋ Total RAM: {total_ram / (1024 ** 3):.2f} GB\n"
+                        xxx_pratheek += f"╰⊚ Used RAM: {used_ram / (1024 ** 3):.2f} GB\n"
+                        xxx_pratheek += f"╭⎋ RAM Usage: {ram_percent}%\n"
+                        xxx_pratheek += f"╰⊚ Total ROM: {total_rom / (1024 ** 3):.2f} GB\n"
+                        xxx_pratheek += f"╭⎋ Used ROM: {used_rom / (1024 ** 3):.2f} GB\n"
+                        xxx_pratheek += f"╰⊚ ROM Usage: {disk_percent}%\n"
                         await app.read_chat_history(bot)
                 except FloodWait as e:
                     await asyncio.sleep(e.x)

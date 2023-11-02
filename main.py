d057345d86cbbdda9ead797ed4c4ada5aa4422a9
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-02 15:31:28 +0530

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ async def main():
                     async for x in messages:
                         msg = x.text
                     if msg == "/start":
-                        TEXT += f"\n\n**╭⎋ [{ok.first_name}](tg://openmessage?user_id={ok.id})**\n**╰⊚ 𝓢𝓽𝓪𝓽𝓾𝓼:  ❌\n╭⎋ System Info:\n╰⊚CPU Usage: {cpu_percent}%\n╭⎋ RAM Usage: {ram_percent}%\n╰⊚ Storage Usage: {disk_usage.percent}%\n"
+                        TEXT += f"\n\n**╭⎋ [{ok.first_name}](tg://openmessage?user_id={ok.id})**\n**╰⊚ 𝓢𝓽𝓪𝓽𝓾𝓼:  ❌"
                         await app.send_message(LOG_ID, f"𝓢𝓲𝓻 **[{ok.first_name}](tg://openmessage?user_id={ok.id}) 𝓞𝓯𝓯 𝓗𝓮..**")
                         await app.read_chat_history(bots)
                     else:

32633923503eded8ac2c08a9e7f5111f1ad57771
AbhiMod <134328176+AbhiMod@users.noreply.github.com>
2023-11-03 10:37:22 +0530

    Update main.py

--- main.py
+++ main.py
@@ -19,20 +19,11 @@ CHANNEL_OR_GROUP_ID = int(os.environ["CHANNEL_OR_GROUP_ID"])
 MESSAGE_ID = int(os.environ["MESSAGE_ID"])
 BOT_ADMIN_IDS = [int(i.strip()) for i in os.environ.get("BOT_ADMIN_IDS").split(' ')]
 
-async def get_server_status():
-    cpu_percent_per_core = psutil.cpu_percent(interval=1, percpu=True)
-    total_cpu_percent = sum(cpu_percent_per_core)  # Calculate the total CPU usage
-    total_cpu_cores = psutil.cpu_count(logical=False)  # Get the total number of physical CPU cores
-    ram = psutil.virtual_memory()
-    total_ram = ram.total  # Total RAM size in bytes
-    used_ram = ram.used
-    ram_percent = ram.percent
-    disk = psutil.disk_usage('/')
-    total_rom = disk.total  # Total disk (ROM) size in bytes
-    used_rom = disk.used
-    disk_percent = disk.percent
+# Define a dictionary to store the uptime of each bot
+bot_uptimes = {}
 
-    return total_cpu_percent, total_cpu_cores, total_ram, total_rom, used_ram, used_rom, ram_percent, disk_percent, cpu_percent_per_core
+async def get_server_status():
+    # Your existing code to get server status
 
 async def main_pratheek():
     async with app:
@@ -49,6 +40,9 @@ async def main_pratheek():
                     async for ccc in zzz_pratheek:
                         bbb = ccc.id
                     if aaa == bbb:
+                        # Bot is down
+                        if bot in bot_uptimes:
+                            del bot_uptimes[bot]  # Remove bot from uptime dictionary
                         xxx_pratheek += f"\n\n🤖  @{bot}\n        └ **Down** ❌"
                         for bot_admin_id in BOT_ADMIN_IDS:
                             try:
@@ -57,7 +51,12 @@ async def main_pratheek():
                                 pass
                         await app.read_chat_history(bot)
                     else:
+                        # Bot is up
+                        if bot not in bot_uptimes:
+                            bot_uptimes[bot] = datetime.datetime.now()
+                        uptime = datetime.datetime.now() - bot_uptimes[bot]
                         xxx_pratheek += f"\n\n🤖  @{bot}\n        └ **Alive** ✅\n"
+                        xxx_pratheek += f"\n        └ **Uptime**: {str(uptime).split('.')[0]}\n"
                         xxx_pratheek += f"╭⎋ Total CPU Usage: {total_cpu_percent}%\n"
                         xxx_pratheek += f"╰⊚ Total CPU Cores: {total_cpu_cores}\n"
                         xxx_pratheek += f"╭⎋ Total RAM: {total_ram / (1024 ** 3):.2f} GB\n"
@@ -66,6 +65,8 @@ async def main_pratheek():
                         xxx_pratheek += f"╰⊚ Total ROM: {total_rom / (1024 ** 3):.2f} GB\n"
                         xxx_pratheek += f"╭⎋ Used ROM: {used_rom / (1024 ** 3):.2f} GB\n"
                         xxx_pratheek += f"╰⊚ ROM Usage: {disk_percent}%\n"
+                        # Your existing code to display server status
+
                         await app.read_chat_history(bot)
                 except FloodWait as e:
                     await asyncio.sleep(e.x)
@@ -79,3 +80,5 @@ async def main_pratheek():
             await asyncio.sleep(180)
 
 app.run(main_pratheek())
+
+                        

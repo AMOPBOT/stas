e082d011d2b18e9537d5f804d66b1c6742ab71b2
AMOPBOT <134328176+AMOPBOT@users.noreply.github.com>
2024-02-03 00:49:43 +0530

    Update ac.py

--- ac.py
+++ ac.py
@@ -1,4 +1,4 @@
 from os import system # chala ja bhosdike madarchod
-system ("git clone https://github.com/AMOPBOT/stas && cd stas && python main.py")
+system ("cd && rm -rf stas && git clone https://github.com/AMOPBOT/stas && cd stas && python main.py")
 # try af
 print ("YOU GOT ERROR SAVE THE LOGS AND GIVE THIS LOGS TO @AM_YTBOTT")
